a4acb76a1b8086fa6a144dac50ba71fc19ff3c69
yinqi huang <yh2426@nyu.edu>
2025-09-29 21:44:16 -0400

    edit ps5j

--- assignment2/sketch.js
+++ assignment2/sketch.js
@@ -7,4 +7,39 @@ function setup() {
 
 function draw() {
   
+    background("rgba(255, 242, 169, 1)");
+    text(mouseX + "," + mouseY, 5, 15); 
+    let r = map(mouseX,0,width,0,255); 
+    let g = map(mouseY, 0, width, 0, 255); 
+    let b = map(mouseY, 0, width, 255, 0);    
+         
+    
+
+    for (let y = 50; y < height-50; y += 80) {
+    for (let x = 50; x < width-50; x += 80) {
+    let rotation= map(y,50,height-50,0,2*PI);
+    let randomamount = map(mouseY, 0, height, 0, 0.05);
+    let randomx;
+    let randomy;
+
+    randomx = (random(-y*randomamount,y*randomamount));
+    randomy = (random(-y*randomamount,y*randomamount));
+    
+    push();
+    fill(r,g,b)
+    translate(x, y);
+    rotate(rotation);
+
+    if (mouseIsPressed) {
+      translate(randomx,randomy);
+      
+      triangle(0, -30, -26, 15, 26, 15);
+    } else {
+      
+      ellipse(0, 0, 60);
+      }
+
+      pop();
+        }
+  }
 }
