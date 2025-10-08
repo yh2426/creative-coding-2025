7e0bd49c6bb65d97f5794db83b948b6807ade0b7
yinqi huang <yh2426@nyu.edu>
2025-10-08 01:45:43 -0400

    Update sketch.js

--- assignment3/sketch.js
+++ assignment3/sketch.js
@@ -1,4 +1,16 @@
 
+//This project is about Time as Water in a Measuring tube
+//so the tube and the trapezoid represent the mesuring tube
+//This clock is inspired by the memory of high school science experiments 
+//when liqluid was carefully poured into a measuring cup to observe changes.
+//time itself becomes that liquid.
+//second(): controls a single droplet that falls down
+//minute(): controls the rising blue bar, representing the gradual filling of the tube.
+//hour(): adds small drops resting in the trapezoid base, representing time that has settled — the past hours.
+//I am still considering whether to include 24 drops in total or just 12, representing AM and PM.
+//Maybe changing the background color to reflect day and night and add some graph
+//Also I am thinking of adding a faucet on top and a flame at the bottom. 
+// The faucet would show time flowing in; the flame shows time being consumed. I am sill try to fingure out how to draw it.
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -11,62 +23,61 @@ function draw() {
   //show current mouse coordinates
   text(mouseX + "," + mouseY, 5, 15); 
 
-  //Tube
+  //draw Tube 
   
   let tubewidth = width*0.1;//let the tube width change based on the screen width.
-  let tubeheight = height/4;
-  let tubex = width/2-tubewidth/2
-  let tubey = height/2.5-tubeheight/2;
-  let ballsize = tubewidth*3;
-  //contaner
-  //  梯形底座（替代圆圈）
-  noStroke();
-  fill("rgba(51, 113, 228, 1)");
+  let tubeheight = height/4;//tube height change based on the screen height
+  let tubex = width/2-tubewidth/2//tubex coordinate
+  let tubey = height/2.5-tubeheight/2;//tube y
+  fill(255);
+  rect(tubex,tubey,tubewidth,tubeheight); 
+  let bottomY = tubey + tubeheight; 
+ 
+  //trap
 
-  let traptop = tubey + tubeheight; // 梯形上边Y（接管子底）
-  let trapheight = tubeheight*0.5; // 梯形高度
-  let trapbottom = traptop + trapheight; // 梯形底边Y
-  let trapleftx = tubex - tubewidth;// 左下角X
-  let traprightx = tubex + tubewidth + tubewidth;  // 右下角X
+  noStroke();
+  fill("rgba(51, 113, 228, 1)");//color
+
+  let traptop = tubey + tubeheight; //traptop top height starting point
+  let trapheight = tubeheight*0.5; // trap height based on the tubehight. 
+  let trapbottom = traptop + trapheight; // trap bottom coordinate
+  let trapleftx = tubex - tubewidth;// trap bottom left point
+  let traprightx = tubex + tubewidth + tubewidth;  // trap bottom right point
 
   beginShape();
-  vertex(tubex, traptop);             // 左上角（管子左边）
-  vertex(tubex + tubewidth, traptop); // 右上角（管子右边）
-  vertex(traprightx, trapbottom);     // 右下角
-  vertex(trapleftx, trapbottom);      // 左下角
+  vertex(tubex, traptop);             // topleft
+  vertex(tubex + tubewidth, traptop); // topright
+  vertex(traprightx, trapbottom);     // bottomright
+  vertex(trapleftx, trapbottom);      // bottomleft
   endShape(CLOSE);
 
   
-  fill(255);
-  rect(tubex,tubey,tubewidth,tubeheight); 
-  
-  let bottomY = tubey + tubeheight; 
 
-
-  //minbar
+  //minbar, represent minute
 
   let m = minute();
   let minbarwidth = tubewidth;
-  let minheight = map(m, 0, 59, 0, tubeheight);
+  let minheight = map(m, 0, 59, 0, tubeheight);//strat from 0 to 59.
   let minbarX = tubex; 
-  let minbarY = bottomY - minheight;
+  let minbarY = bottomY - minheight;//start from the bottom minues the height, so the water will increase
 
   fill("rgba(51, 113, 228, 1)");
   rect(minbarX, minbarY, minbarwidth, minheight);
 
-// 💧 秒 - 改成水珠
+  //drop
   let s = second();
 
-// Y 位置根据秒从上到下移动
+  // y coordinate from top to bottom
   let dropY = map(s, 0, 59, tubey, bottomY);
 
-// 水珠X在量杯内部中间
+  // drop should be inside the tube
   let dropX = tubex + tubewidth/2;
 
-// 画水珠
+  //drop
   noStroke();
   fill("rgba(113, 197, 245, 1)");
-  ellipse(dropX, dropY, tubewidth/5); // 水珠大小为量杯宽度的 1/5
+  ellipse(dropX, dropY, tubewidth/5); // the size of the drop is 1/5 size of the tube width
+
 
 
   stroke(255);
@@ -78,16 +89,16 @@ function draw() {
   push();
  
   for (let i = 0; i <= 60; i += 10) {
-    let mintexty = map(i, 0, 59, bottomY, tubey);
-    let sectexty = map(i, 0, 59, tubey, bottomY);
+    let mintexty = map(i, 0, 59, bottomY, tubey);//from bottom to top
+    let sectexty = map(i, 0, 59, tubey, bottomY);//from top to bottom
 
-    // 左边
+    //left side text
     textAlign(RIGHT);
     fill(255);
     text(i, tubex - 30, sectexty + 5);
     line(tubex - 20, sectexty, tubex - 5, sectexty);
 
-    // 右边mintext
+    //right side text
     textAlign(LEFT);
     text(i, tubex + tubewidth + 30, mintexty + 5);
     line(tubex + tubewidth + 5, mintexty, tubex + tubewidth + 20, mintexty);
@@ -95,20 +106,20 @@ function draw() {
   }
   pop();
   
-// 💧 小时代表的水珠
-  let h = hour(); // 获取当前小时
-  let dropCount = h; // 最多12颗
+// drops represent hour
+  let h = hour(); //current hour
+  let dropCount = h; 
 
-// 固定随机种子，让同一个小时内水珠位置不会闪动
-  randomSeed(h);
+// 
+  randomSeed(h);//fixes the random sequence so that the drop will remain in the same position
   noStroke();
 
-// 在底部圆圈范围内随机生成水珠
+// random drop
   for (let i = 0; i < dropCount; i++) {
-    let x = random(tubex - tubewidth*0.3, tubex + tubewidth+tubewidth*0.3); // 随机x位置（瓶子宽度范围）
-    let y = random(bottomY+tubeheight*0.3, bottomY + tubeheight*0.4); // 随机y位置（瓶底圆圈范围）
+    let x = random(tubex - tubewidth*0.3, tubex + tubewidth+tubewidth*0.3); // inside the trapl width
+    let y = random(bottomY+tubeheight*0.3, bottomY + tubeheight*0.4); // inside the trap height
     fill("rgba(113, 197, 245, 1)");
-    ellipse(x, y, tubewidth/5); // 水珠大小
+    ellipse(x, y, tubewidth/5);
   }
 }
  
